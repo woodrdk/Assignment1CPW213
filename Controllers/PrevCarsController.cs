3bc246ae9b47c1f3cc9badda36b6dbcb25f49b65
Rob Wood <rdrwood@gmail.com>
2018-07-10 12:45:56 -0700

    Initial commit

---
+++ Controllers/PrevCarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1CPW213.Controllers
+{
+    public class PrevCarsController : Controller
+    {
+        // GET: PrevCars
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment1CPW213.MvcApplication" Language="C#" %>
